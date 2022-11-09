7c84a6857ff1af17aa862790672802de48ab9ea4
sowmyagoud12 <sowmya.yf78@gmail.com>
2022-11-08 22:54:27 -0600

    added try catch

--- controllers/television.js
+++ controllers/television.js
@@ -1,26 +1,33 @@
 var television = require('../models/television'); 
  
-// List of all Costumes 
-exports.television_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Television list'); 
+// List of all television 
+exports.television_list = async function(req, res) { 
+    try{ 
+        theTelevision = await television.find(); 
+        res.send(theTelevision); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
-// for a specific Costume. 
+// for a specific Television. 
 exports.television_detail = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television detail: ' + req.params.id); 
 }; 
  
-// Handle Costume create on POST. 
+// Handle Television create on POST. 
 exports.television_create_post = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television create POST'); 
 }; 
  
-// Handle Costume delete form on DELETE. 
+// Handle Television delete form on DELETE. 
 exports.television_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Costume update form on PUT. 
+// Handle Television update form on PUT. 
 exports.television_update_put = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television update PUT' + req.params.id); 
 }; 
