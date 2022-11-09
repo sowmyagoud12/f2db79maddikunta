f0a21589e9e76912980e7fe777dcf7c4f038b23a
sowmyagoud12 <sowmya.yf78@gmail.com>
2022-11-08 23:50:49 -0600

    updated television

--- controllers/television.js
+++ controllers/television.js
@@ -25,16 +25,33 @@ exports.television_view_all_Page = async function(req, res) {
     }   
 }; 
 
+// Handle Costume create on POST. 
+exports.television_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new television(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"tele_name":"Visio", "size":"90 inch", "cost":"800"} 
+    document.tele_name = req.body.tele_name;    
+    document.size = req.body.size;
+    document.cost = req.body.cost;  
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+
 // for a specific Television. 
 exports.television_detail = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television detail: ' + req.params.id); 
 }; 
  
-// Handle Television create on POST. 
-exports.television_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Television create POST'); 
-}; 
- 
+
 // Handle Television delete form on DELETE. 
 exports.television_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television delete DELETE ' + req.params.id); 
