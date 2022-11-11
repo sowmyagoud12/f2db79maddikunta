53f095c616f5171b652482e6fb6df262128e0dd4
sowmyagoud12 <sowmya.yf78@gmail.com>
2022-11-10 21:56:52 -0600

    updated put get methods

--- controllers/television.js
+++ controllers/television.js
@@ -47,17 +47,37 @@ exports.television_create_post = async function(req, res) {
 }; 
 
 // for a specific Television. 
-exports.television_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Television detail: ' + req.params.id); 
+exports.television_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await television.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
- 
-
+//Handle Costume update form on PUT. 
+exports.television_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await television.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.tele_name)  
+               toUpdate.tele_name = req.body.tele_name; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+                
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
+}; 
 // Handle Television delete form on DELETE. 
 exports.television_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Television delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Television update form on PUT. 
-exports.television_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Television update PUT' + req.params.id); 
-}; 
--- routes/television.js
+++ routes/television.js
@@ -2,6 +2,8 @@ var express = require('express');
 const television_controlers= require('../controllers/television');
 var router = express.Router();
 
-/* GET costumes */ 
+/* GET television */ 
 router.get('/', television_controlers.television_view_all_Page ); 
+// GET request for one Television. 
+router.get('/television/:id', television_controlers.television_detail); 
 module.exports = router; 
