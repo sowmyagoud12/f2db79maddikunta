4380ae4a3e7cdc1a5c953bf3cce81b7c282bc3d9
sowmyagoud12 <sowmya.yf78@gmail.com>
2022-11-29 14:00:33 -0600

    added security

--- routes/television.js
+++ routes/television.js
@@ -1,6 +1,16 @@
 var express = require('express');
 const television_controlers= require('../controllers/television');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 
 /* GET television */ 
 router.get('/', television_controlers.television_view_all_Page ); 
@@ -11,7 +21,7 @@ router.get('/detail', television_controlers.television_view_one_Page);
 /* GET create costume page */ 
 router.get('/create', television_controlers.television_create_Page); 
 /* GET create update page */ 
-router.get('/update', television_controlers.television_update_Page);
+router.get('/update',secured, television_controlers.television_update_Page);
 /* GET delete costume page */ 
 router.get('/delete', television_controlers.television_delete_Page); 
 module.exports = router; 
